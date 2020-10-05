11683d0f77cb49dc943fedcd12d6451b68fd9dc8
ToBeNew <crebiz76@gmail.com>
2020-10-05 20:31:19 +0900

    Chapter01-section1

---
+++ Main.java
@@ -0,0 +1,12 @@
+// [바로실행] 생활코딩 - 자바(JAVA)
+
+// 01 - 언어 소개 
+class Chapter01
+{ 
+  // 1. Greeting
+  public static void section1()
+  {
+    // Self-test
+    System.out.println("Hello world!");
+  }
+}
