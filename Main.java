11bd1b4f98a693f0541b1740a97a2253574791b9
ToBeNew <crebiz76@gmail.com>
2020-10-05 21:31:43 +0900

    Chapter02-section3

--- Main.java
+++ Main.java
@@ -3,13 +3,26 @@ class Main
 {
   public static void main(String[] args)
   {
-    Chapter02.section2();
+    Chapter02.section3();
   }
 }
 
 // 02 - 숫자와 문자 
 class Chapter02
-{   
+{
+  // 3. 문자와 문자열
+  public static void section3()
+  {
+    System.out.println('생');
+    System.out.println("생활코딩");
+    //문자열을 작은따옴표로 감싸면 에러가 난다.
+    //System.out.println('생활코딩');
+    System.out.println("생");
+    System.out.println("egoing said \"Welcome programming world\"");
+    System.out.println("HTML\nCSS\nJavaScript\n");
+    System.out.println("생활"+"코딩");
+  }
+      
   // 2. 숫자
   public static void section2()
   {
