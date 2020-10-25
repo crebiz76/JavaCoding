44540af670d8a31843bee80e4663ac66760f921d
ToBeNew <crebiz76@gmail.com>
2020-10-26 01:35:49 +0900

    Chapter37-section117

--- Main.java
+++ Main.java
@@ -10,7 +10,35 @@ class Main
 {
 	public static void main(String[] args)
 	{
-		Chapter36.section116();
+		Chapter37.section117();
+	}
+}
+
+// 37 - 상수와 enum
+class Chapter37
+{
+	private final static int APPLE = 1;
+	private final static int PEACH = 2;
+	private final static int BANANA = 3;
+
+	// 117. 상수에 대한 복습
+	public static void section117()
+	{
+		int type = APPLE;
+		switch(type)
+		{
+			case APPLE:
+			System.out.println(57+"kcal");
+			break;
+
+			case PEACH:
+			System.out.println(34+"kcal");
+			break;
+
+			case BANANA:
+			System.out.println(93+"kcal");
+			break;
+		}
 	}
 }
 
