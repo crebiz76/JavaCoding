d2d30ba2a013798a4457314fa37b216380653b0e
ToBeNew <crebiz76@gmail.com>
2020-10-28 01:03:10 +0900

    Chapter40-section135

--- Main.java
+++ Main.java
@@ -8,6 +8,9 @@ import java.util.Map;
 import java.util.HashSet;	// As for Chapter40.section132
 import java.util.HashMap;	// As for Chapter40.section133
 import java.util.Set;		// As for Chapter40.section134
+import java.util.List;		// As for Chapter40.section135
+import java.util.Collections;	// As for Chapter40.section135
+import java.lang.Comparable; 	// As for Chapter40.section135
 
 import srcbin.*;			// As for Chapter26.*
 
@@ -15,14 +18,57 @@ class Main
 {
 	public static void main(String[] args)
 	{
-		Chapter40.section134();
+		Chapter40.section135();
+	}
+}
+
+// 40 - Collection Framework(예제 135)
+class Computer implements Comparable<Object>
+{
+	int serial;
+	String owner;
+	Computer(int serial, String owner)
+	{
+		this.serial = serial;
+		this.owner = owner;
+	}
+	public int compareTo(Object o)
+	{
+		return this.serial - ((Computer)o).serial;
+	}
+	public String toString()
+	{
+		return serial + " " + owner;
 	}
 }
 
 // 40 - Collection Framework
 class Chapter40
 {
-	// 134. Map(예제 134)
+	// 135. 정렬
+	public static void section135()
+	{
+		List<Computer> computers = new ArrayList<Computer>();
+		computers.add(new Computer(500, "egoing"));
+		computers.add(new Computer(200, "leeZche"));
+		computers.add(new Computer(3233, "graphittie"));
+		Iterator<Computer> i = computers.iterator();
+		System.out.println("before");
+		while(i.hasNext())
+		{
+			System.out.println(i.next());
+		}
+
+		Collections.sort(computers);
+		System.out.println("\nafter");
+		i = computers.iterator();
+		while(i.hasNext())
+		{
+			System.out.println(i.next());
+		}
+	}
+
+	// 134. Map(예제)
 	static void iteratorUsingForEach(HashMap<String, Integer> map)
 	{
 		Set<Map.Entry<String, Integer>> entries = map.entrySet();
